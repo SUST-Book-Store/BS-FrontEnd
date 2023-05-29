f15a5a501465595fe4598e4d54102b1ec3f6d366
Shiona <xapenny@icloud.com>
2023-05-30 00:59:37 +0800

    修复Token过期逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -1,156 +1,157 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from "@/views/HomeView.vue"
-import UserCenterview from "@/views/UserCenterView.vue"
-import CartView from "@/views/CartView.vue"
-import OrderView from "@/views/OrderView.vue"
-import UserLoginView from "@/views/UserLoginView.vue"
-import UserRegisterView from "@/views/UserRegisterView.vue"
-import NotFound from "@/views/NotFound.vue"
-import DetailView from "@/views/DetailView.vue"
-import OrderDetailView from "@/views/OrderDetailView.vue"
-import AdminBookView from "@/views/AdminBookView.vue"
-import EditBookView from '@/views/EditBookView.vue'
-import AdminUser from '@/views/AdminUser'
-import AddBookView from '@/views/AddBookView'
-import AdminOrdeView from '@/views/AdminOrdeView'
-import SearchView from '@/views/SearchView'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import UserCenterview from "@/views/UserCenterView.vue";
+import CartView from "@/views/CartView.vue";
+import OrderView from "@/views/OrderView.vue";
+import UserLoginView from "@/views/UserLoginView.vue";
+import UserRegisterView from "@/views/UserRegisterView.vue";
+import NotFound from "@/views/NotFound.vue";
+import DetailView from "@/views/DetailView.vue";
+import OrderDetailView from "@/views/OrderDetailView.vue";
+import AdminBookView from "@/views/AdminBookView.vue";
+import EditBookView from "@/views/EditBookView.vue";
+import AdminUser from "@/views/AdminUser";
+import AddBookView from "@/views/AddBookView";
+import AdminOrdeView from "@/views/AdminOrdeView";
+import SearchView from "@/views/SearchView";
+import { ElMessage } from "element-plus";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    redirect: "/home/",
-    meta: { title: '首页 - 网上书店' }
-  },
-  {
-    path: "/home/",
-    name: "home_index",
-    component: HomeView,
-    meta: { title: '首页 - 网上书店' }
-  },
-  {
-    path: "/search/:keyword",
-    name: "search",
-    component: SearchView,
-    meta: { title: '搜索 - 网上书店' }
-  },
-  {
-    path: "/cart/",
-    name: "cart_index",
-    component: CartView,
-    meta: { title: '购物车 - 网上书店' }
-  },
-  {
-    path: "/user/center/",
-    name: "user_center_index",
-    component: UserCenterview,
-    meta: { title: '个人中心 - 网上书店' }
-  },
-  {
-    path: "/order/",
-    name: "order_index",
-    component: OrderView,
-    meta: { title: '我的订单 - 网上书店' }
-  },
-  {
-    path: "/user/login/",
-    name: "user_login_index",
-    component: UserLoginView,
-    meta: { title: '登录 - 网上书店' },
-    beforeEnter: (to, from, next) => {
-    // 判断用户是否已登录
-    const isLogin = localStorage.getItem('token')
-    if (isLogin) {
-      // 如果已登录，则自动跳转到首页
-      next({ path: '/' })
-    } else {
-      // 如果未登录，则继续进入登录页
-      next()
+    {
+        path: "/",
+        name: "home",
+        redirect: "/home/",
+        meta: { title: "首页 - 网上书店" }
+    },
+    {
+        path: "/home/",
+        name: "home_index",
+        component: HomeView,
+        meta: { title: "首页 - 网上书店" }
+    },
+    {
+        path: "/search/:keyword",
+        name: "search",
+        component: SearchView,
+        meta: { title: "搜索 - 网上书店" }
+    },
+    {
+        path: "/cart/",
+        name: "cart_index",
+        component: CartView,
+        meta: { title: "购物车 - 网上书店" }
+    },
+    {
+        path: "/user/center/",
+        name: "user_center_index",
+        component: UserCenterview,
+        meta: { title: "个人中心 - 网上书店" }
+    },
+    {
+        path: "/order/",
+        name: "order_index",
+        component: OrderView,
+        meta: { title: "我的订单 - 网上书店" }
+    },
+    {
+        path: "/user/login/",
+        name: "user_login_index",
+        component: UserLoginView,
+        meta: { title: "登录 - 网上书店" },
+        beforeEnter: (to, from, next) => {
+            // 判断用户是否已登录
+            const isLogin = localStorage.getItem("token");
+            if (isLogin) {
+                // 如果已登录，则自动跳转到首页
+                ElMessage.warning("您已登陆");
+                next({ path: "/" });
+            } else {
+                // 如果未登录，则继续进入登录页
+                next();
+            }
+        }
+    },
+    {
+        path: "/user/register/",
+        name: "user_register_index",
+        component: UserRegisterView,
+        meta: { title: "注册 - 网上书店" },
+        beforeEnter: (to, from, next) => {
+            // 判断用户是否已登录
+            const isLogin = localStorage.getItem("token");
+            if (isLogin) {
+                // 如果已登录，则自动跳转到首页
+                next({ path: "/" });
+            } else {
+                // 如果未登录，则继续进入登录页
+                next();
+            }
+        }
+    },
+    {
+        path: "/book/detail/",
+        name: "book_detail_index",
+        component: DetailView,
+        meta: { title: "商品详情 - 网上书店" }
+    },
+    {
+        path: "/order/detail/",
+        name: "order_detail_index",
+        component: OrderDetailView,
+        meta: { title: "订单详情 - 网上书店" }
+    },
+    {
+        path: "/admin/manageUser",
+        name: "useradmin",
+        component: AdminUser,
+        meta: { title: "用户管理 - 网上书店" }
+    },
+    {
+        path: "/admin/index",
+        name: "adminindex",
+        component: AdminBookView,
+        meta: { title: "书籍管理 - 网上书店" }
+    },
+    {
+        path: "/admin/editBook",
+        name: "EditBook",
+        component: EditBookView,
+        props: true,
+        meta: { title: "书籍编辑 - 网上书店" }
+    },
+    {
+        path: "/admin/manageOrder",
+        name: "AdminOrder",
+        component: AdminOrdeView,
+        meta: { title: "订单管理 - 网上书店" }
+    },
+    {
+        path: "/admin/addBook",
+        name: "AddBook",
+        component: AddBookView,
+        meta: { title: "添加书籍 - 网上书店" }
+    },
+    {
+        path: "/404/",
+        name: "404",
+        component: NotFound,
+        meta: { title: "页面未找到 - 网上书店" }
+    },
+    {
+        path: "/:catchAll(.*)",
+        redirect: "/404/"
     }
-  }
-  },
-  {
-    path: "/user/register/",
-    name: "user_register_index",
-    component: UserRegisterView,
-    meta: { title: '注册 - 网上书店' },
-    beforeEnter: (to, from, next) => {
-    // 判断用户是否已登录
-    const isLogin = localStorage.getItem('token')
-    if (isLogin) {
-      // 如果已登录，则自动跳转到首页
-      next({ path: '/' })
-    } else {
-      // 如果未登录，则继续进入登录页
-      next()
-    }
-  }
-  },
-  {
-    path: "/book/detail/",
-    name: "book_detail_index",
-    component: DetailView,
-    meta: { title: '商品详情 - 网上书店' }
-  },
-  {
-    path: "/order/detail/",
-    name: "order_detail_index",
-    component: OrderDetailView,
-    meta: { title: '订单详情 - 网上书店' }
-  },
-  {
-    path: "/admin/manageUser",
-    name: "useradmin",
-    component: AdminUser,
-    meta: { title: '用户管理 - 网上书店' }
-  },
-  {
-    path: '/admin/index',
-    name: 'adminindex',
-    component:AdminBookView,
-    meta: { title: '书籍管理 - 网上书店' }
-  },
-  {
-    path: "/admin/editBook",
-    name: "EditBook",
-    component: EditBookView,
-    props: true,
-    meta: { title: '书籍编辑 - 网上书店' }
-  },
-  {
-    path: "/admin/manageOrder",
-    name: "AdminOrder",
-    component: AdminOrdeView,
-    meta: { title: '订单管理 - 网上书店' }
-  },
-  {
-    path: "/admin/addBook",
-    name: "AddBook",
-    component: AddBookView,
-    meta: { title: '添加书籍 - 网上书店' }
-  },
-  {
-    path: "/404/",
-    name: "404",
-    component: NotFound,
-    meta: { title: '页面未找到 - 网上书店' }
-  },
-  {
-    path: "/:catchAll(.*)",
-    redirect: "/404/"
-  }
-  
-]
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+    history: createWebHistory(),
+    routes
+});
 
 router.beforeEach(async (to) => {
     if (to.meta.title) {
         document.title = to.meta.title;
     }
-})
+});
 
-export default router
+export default router;
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,61 +1,64 @@
-import axios from 'axios'
+import axios from "axios";
 import router from "../router";
-import {ElMessage} from "element-plus";
-import store from '@/store';
+import { ElMessage } from "element-plus";
+import store from "@/store";
 
 const request = axios.create({
-    baseURL: '/',
+    baseURL: "/",
     timeout: 5000
-})
+});
 
 // request 拦截器
 // 可以自请求发送前对请求做一些处理
 // 比如统一加token，对请求参数统一加密
-request.interceptors.request.use(config => {
-    config.headers['Content-Type'] = 'application/json;charset=utf-8';
-    // 判断url是否包含admin/，如果是则跳转首页
-    if (config.url.includes("admin/")) {
-        if (!store.state.user.is_admin){
-            ElMessage.error("你没有权限")
-            router.push("/");
+request.interceptors.request.use(
+    (config) => {
+        config.headers["Content-Type"] = "application/json;charset=utf-8";
+        // 判断url是否包含admin/，如果是则跳转首页
+        if (config.url.includes("admin/")) {
+            if (!store.state.user.is_admin) {
+                ElMessage.error("你没有权限");
+                router.push("/");
+            }
         }
-        
-    }
 
-    // config.headers['token'] = user.token;  // 设置请求头
-    //取出sessionStorage里面缓存的用户信息
-    let userToken = localStorage.getItem("token")
-    if(userToken === 'null' || userToken === '' || userToken === undefined)
-    {
-        router.push("/user/login/")
-    } else {
-        config.headers['token'] = userToken
+        // config.headers['token'] = user.token;  // 设置请求头
+        let userToken = localStorage.getItem("token");
+        if (
+            userToken === "null" ||
+            userToken === "" ||
+            userToken === undefined
+        ) {
+            router.push("/user/login/");
+        } else {
+            config.headers["token"] = userToken;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config
-}, error => {
-    return Promise.reject(error)
-});
+);
 
 request.interceptors.response.use(
-  response => {
-    return response;
-  },
-  error => {
-    if(error.response){
-      switch(error.response.status){
-        case 401:
-          ElMessage.error('未授权，请先登录');
-          sessionStorage.removeItem('token');
-          router.replace({
-            path: '/user/login',
-            query: {
-              redirect: router.currentRoute.fullPath //登录成功后跳入浏览的当前页
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response) {
+            switch (error.response.status) {
+                case 401:
+                    ElMessage.error("未授权，请先登录");
+                    localStorage.removeItem("token");
+                    router.replace({
+                        path: "/user/login",
+                        query: {
+                            redirect: router.currentRoute.fullPath //登录成功后跳入浏览的当前页
+                        }
+                    });
             }
-          })
-      }
+        }
     }
-  }
-)
+);
 
-export default request
-
+export default request;
